7cb88a348300f386df66d951b7944c333c055ef3
Lucasor99 <luiscastanoortega@gmail.com>
2024-07-27 16:54:03 +0200

    .

--- Auto_GS/Auto_GS/settings.py
+++ Auto_GS/Auto_GS/settings.py
@@ -90,20 +90,20 @@ WSGI_APPLICATION = 'Auto_GS.wsgi.application'
 
 DATABASES = {
 
-    # 'default': {
-    #     'NAME': 'user_data',
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'USER': 'admin',
-    #     'PASSWORD': os.getenv('MYSQL_ROOT_PASSWORD'),
-    #     'HOST': 'mysql',
-    #     #'HOST': 'mysql.default.svc.cluster.local',
-    #     'PORT': '3306',
-    # },
-
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'user_data',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'admin',
+        'PASSWORD': os.getenv('MYSQL_ROOT_PASSWORD'),
+        'HOST': 'mysql',
+        #'HOST': 'mysql.default.svc.cluster.local',
+        'PORT': '3306',
     },
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
 
     'cassandra': {
         'ENGINE': 'django_cassandra_engine',
